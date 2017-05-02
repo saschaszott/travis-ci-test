08efff39ce38d692b507959a378233df143ab34f
Sascha Szott <szott@gmx.de>
2017-05-02 17:59:46 +0200

    Create EmailTest.php

---
+++ test/EmailTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase {
+
+    public function testCanBeCreatedFromValidEmailAddress() {
+        $this->assertInstanceOf(Email::class, Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress() {
+        $this->expectException(InvalidArgumentException::class);
+        Email::fromString('invalidemailaddress');
+    }
+
+    public function testCanBeUsedAsString() {
+        $this->assertEquals('user@example.com', Email::fromString('user@example.com')
+        );
+    }
+}
